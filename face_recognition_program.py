dbf176957d3a405c2076f9f5259bc32166afe169
TheKidKy <imsajadky@gmail.com>
2023-12-24 10:14:28 +0330

    updated get_face_encoding()

--- face_recognition_program.py
+++ face_recognition_program.py
@@ -3,7 +3,7 @@ import numpy as np # to handle all lists/arrays
 import cv2 # to capture footage
 import os # to handle all matters relating to folders, paths, image/file names, etc.
 
-faces_path = "~/Desktop/faces/known" # replace your own folder path
+faces_path = "~\\Desktop\\faces\\known" # replace your own folder path
 
 
 # Function to get face names, as well as face encodings
@@ -11,3 +11,11 @@ def get_face_encoding():
     face_names = os.listdir(f"{faces_path}\\known")
     face_encoding = []
 
+    # retrieves all face encodings and store them in a list
+    for i, name in enumerate(face_names):
+        face = fr.load_image_file(f"{faces_path}\\known\\{name}")
+        face_encoding.append(fr.face_encodings(face)[0])
+
+        face_names[i] = name.split(".")[0] # to remove ".jpg" or any other image extension
+
+    return face_encoding, face_names
